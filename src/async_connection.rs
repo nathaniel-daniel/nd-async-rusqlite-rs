4140af4e57e6d1d53e5d659d7685289e0b0e4d42
Nathaniel Daniel <nathaniel.daniel12@gmail.com>
2023-09-09 19:27:14 -0700

    Move builder to new file

--- src/async_connection.rs
+++ src/async_connection.rs
@@ -1,7 +1,8 @@
+mod builder;
+
+pub use self::builder::AsyncConnectionBuilder;
 use crate::Error;
 use crate::SyncWrapper;
-use std::path::Path;
-use std::path::PathBuf;
 
 enum Message {
     Access {
@@ -91,105 +92,5 @@ impl AsyncConnection {
         let rx = self.access_internal(func)?;
         let result = rx.blocking_recv().map_err(|_| Error::Aborted)??;
         Ok(result)
-    }
-}
-
-/// A builder for an [`AsyncConnection`].
-#[derive(Debug)]
-pub struct AsyncConnectionBuilder {}
-
-impl AsyncConnectionBuilder {
-    /// Create an [`AsyncConnectionBuilder`] with default settings.
-    pub fn new() -> Self {
-        Self {}
-    }
-
-    fn open_internal(
-        &self,
-        path: PathBuf,
-    ) -> (
-        AsyncConnection,
-        tokio::sync::oneshot::Receiver<Result<(), rusqlite::Error>>,
-    ) {
-        let (tx, rx) = std::sync::mpsc::channel::<Message>();
-        let (connection_open_tx, connection_open_rx) = tokio::sync::oneshot::channel();
-        std::thread::spawn(move || {
-            let mut connection = match rusqlite::Connection::open(path) {
-                Ok(connection) => {
-                    // Check if the user cancelled the opening of the database connection and return early if needed.
-                    if connection_open_tx.send(Ok(())).is_err() {
-                        return;
-                    }
-
-                    connection
-                }
-                Err(error) => {
-                    // Don't care if we succed since we should exit in either case.
-                    let _ = connection_open_tx.send(Err(error)).is_ok();
-                    return;
-                }
-            };
-
-            let mut close_tx = None;
-            while let Ok(message) = rx.recv() {
-                match message {
-                    Message::Close { tx } => {
-                        close_tx = Some(tx);
-                        break;
-                    }
-                    Message::Access { func } => {
-                        func(&mut connection);
-                    }
-                }
-            }
-
-            // Drop rx.
-            // This will abort all queued messages, dropping them without sending a response.
-            // This is considered aborting the request.
-            drop(rx);
-
-            let result = connection.close();
-            if let Some(tx) = close_tx {
-                let _ = tx
-                    .send(result.map_err(|(_connection, error)| Error::from(error)))
-                    .is_ok();
-            }
-        });
-
-        (AsyncConnection { tx }, connection_open_rx)
-    }
-
-    /// Open the connection.
-    pub async fn open<P>(&self, path: P) -> Result<AsyncConnection, Error>
-    where
-        P: AsRef<Path>,
-    {
-        let path = path.as_ref().to_path_buf();
-
-        let (async_connection, connection_open_rx) = self.open_internal(path);
-        connection_open_rx.await.map_err(|_| Error::Aborted)??;
-
-        Ok(async_connection)
-    }
-
-    /// Open the connection from a blocking context.
-    pub fn blocking_open<P>(&self, path: P) -> Result<AsyncConnection, Error>
-    where
-        P: AsRef<Path>,
-    {
-        let path = path.as_ref().to_path_buf();
-
-        let (async_connection, connection_open_rx) = self.open_internal(path);
-        connection_open_rx
-            .blocking_recv()
-            .map_err(|_| Error::Aborted)??;
-
-        Ok(async_connection)
-    }
-}
-
-impl Default for AsyncConnectionBuilder {
-    fn default() -> Self {
-        Self::new()
     }
 }
---
+++ src/async_connection/builder.rs
@@ -0,0 +1,105 @@
+use super::AsyncConnection;
+use super::Message;
+use crate::Error;
+use std::path::Path;
+use std::path::PathBuf;
+
+/// A builder for an [`AsyncConnection`].
+#[derive(Debug)]
+pub struct AsyncConnectionBuilder {}
+
+impl AsyncConnectionBuilder {
+    /// Create an [`AsyncConnectionBuilder`] with default settings.
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    fn open_internal(
+        &self,
+        path: PathBuf,
+    ) -> (
+        AsyncConnection,
+        tokio::sync::oneshot::Receiver<Result<(), rusqlite::Error>>,
+    ) {
+        let (tx, rx) = std::sync::mpsc::channel::<Message>();
+        let (connection_open_tx, connection_open_rx) = tokio::sync::oneshot::channel();
+        std::thread::spawn(move || {
+            let mut connection = match rusqlite::Connection::open(path) {
+                Ok(connection) => {
+                    // Check if the user cancelled the opening of the database connection and return early if needed.
+                    if connection_open_tx.send(Ok(())).is_err() {
+                        return;
+                    }
+
+                    connection
+                }
+                Err(error) => {
+                    // Don't care if we succed since we should exit in either case.
+                    let _ = connection_open_tx.send(Err(error)).is_ok();
+                    return;
+                }
+            };
+
+            let mut close_tx = None;
+            for message in rx.iter() {
+                match message {
+                    Message::Close { tx } => {
+                        close_tx = Some(tx);
+                        break;
+                    }
+                    Message::Access { func } => {
+                        func(&mut connection);
+                    }
+                }
+            }
+
+            // Drop rx.
+            // This will abort all queued messages, dropping them without sending a response.
+            // This is considered aborting the request.
+            drop(rx);
+
+            let result = connection.close();
+            if let Some(tx) = close_tx {
+                let _ = tx
+                    .send(result.map_err(|(_connection, error)| Error::from(error)))
+                    .is_ok();
+            }
+        });
+
+        (AsyncConnection { tx }, connection_open_rx)
+    }
+
+    /// Open the connection.
+    pub async fn open<P>(&self, path: P) -> Result<AsyncConnection, Error>
+    where
+        P: AsRef<Path>,
+    {
+        let path = path.as_ref().to_path_buf();
+
+        let (async_connection, connection_open_rx) = self.open_internal(path);
+        connection_open_rx.await.map_err(|_| Error::Aborted)??;
+
+        Ok(async_connection)
+    }
+
+    /// Open the connection from a blocking context.
+    pub fn blocking_open<P>(&self, path: P) -> Result<AsyncConnection, Error>
+    where
+        P: AsRef<Path>,
+    {
+        let path = path.as_ref().to_path_buf();
+
+        let (async_connection, connection_open_rx) = self.open_internal(path);
+        connection_open_rx
+            .blocking_recv()
+            .map_err(|_| Error::Aborted)??;
+
+        Ok(async_connection)
+    }
+}
+
+impl Default for AsyncConnectionBuilder {
+    fn default() -> Self {
+        Self::new()
+    }
+}
