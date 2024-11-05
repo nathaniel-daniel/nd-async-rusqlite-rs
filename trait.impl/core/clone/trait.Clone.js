7e1328700109540c9e6360aaaf7745f067e18778
nathaniel-daniel <nathaniel-daniel@users.noreply.github.com>
2024-11-05 20:15:00 +0000

    deploy: 5340073f39d12c40f1cdefbe47d261da514a13d2

---
+++ trait.impl/core/clone/trait.Clone.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"nd_async_rusqlite":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.81.0/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"nd_async_rusqlite/struct.AsyncConnection.html\" title=\"struct nd_async_rusqlite::AsyncConnection\">AsyncConnection</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.81.0/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"nd_async_rusqlite/struct.WalPool.html\" title=\"struct nd_async_rusqlite::WalPool\">WalPool</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ trait.impl/core/error/trait.Error.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"nd_async_rusqlite":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.81.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"nd_async_rusqlite/enum.Error.html\" title=\"enum nd_async_rusqlite::Error\">Error</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
