a61c2275a4830b815837564b3f4d836cd98d2a0f
nathaniel-daniel <nathaniel-daniel@users.noreply.github.com>
2024-12-02 20:03:26 +0000

    deploy: a4c97ab32a78b69771fd922aa62f3d630ad17faa

---
+++ src-files.js
@@ -0,0 +1,3 @@
+var srcIndex = new Map(JSON.parse('[["nd_async_rusqlite",["",[["async_connection",[],["builder.rs"]],["wal_pool",[],["builder.rs"]]],["async_connection.rs","lib.rs","sync_wrapper.rs","wal_pool.rs"]]]]'));
+createSrcSidebar();
+//{"start":36,"fragment_lengths":[163]}
